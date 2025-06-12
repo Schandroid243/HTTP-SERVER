803a34abbb8921170644cfb460e3390bfdb2a494
Schandroid243 <schadrackngunza@gmail.com>
2025-06-12 17:41:21 +0100

    feat(index.js): Added a collection of friends Splited the req.url in order to check if there is a parameterize url which target to a specific friend retrieved the the url parameter and then pass it to our friends collection in order to retrieve the specific friend that we want.

--- index.js
+++ index.js
@@ -4,20 +4,49 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: "Nicolas Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+  {
+    id: 3,
+    name: "Carl Jung",
+  },
+  {
+    id: 4,
+    name: "Schrödinger",
+  },
+  {
+    id: 5,
+    name: "Oppenheimer",
+  },
+  {
+    id: 6,
+    name: "Bill Gates",
+  },
+];
+
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
-    // res.writeHead(200, {
-    //   "Content-Type": "application/json",
-    // });
+  const items = req.url.split("/");
+  if (items[1] === "friends") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: "Sir Isaac Newton",
-      })
-    );
-  } else if (req.url === "/messages") {
+    if (items.length === 3) {
+      const specificFriend = +items[2]; //Shorthand casting for string to number. We can also use Number(items[2]);
+      res.end(JSON.stringify(friends[specificFriend]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "messages") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<body>");
